6116bd5392d4a2371ca1d6ec974f0fab21af6c39
Rocco Scavone <scavonerocco@hotmail.it>
2020-05-28 18:50:23 +0200

    Fixed error range

--- src/main/java/m2ex/S02.java
+++ src/main/java/m2ex/S02.java
@@ -72,11 +72,11 @@ public class S02 {
 	 */
 	public static int score(double x, double y) {
 		double distance = Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2));
-		if(distance < 5) {
+		if(distance > 0 && distance <= 1) {
 			return 10;
-		} else if(distance >= 5 && distance < 10) {
+		} else if(distance > 1 && distance <= 5) {
 			return 5;
-		} else if(distance == 10) {
+		} else if(distance > 5 && distance <= 10) {
 			return 1;
 		} else 
 			return 0;
